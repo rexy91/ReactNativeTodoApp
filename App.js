b70f66d36cb4cde577064bf4f9f4a18bd33ecc97
Yuho <yuho@Yuhos-MBP.home>
2021-02-10 15:18:07 -0500

    ableToSeeappOniPhoneViaExpo

--- App.js
+++ App.js
@@ -1,22 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [toggle, setToggle] = useState(false)
+
+  const ToggleTrueFalseText = () => {
+        setToggle(!toggle)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>This is my React Native Todo App</Text>
+      <Text>React Native Todo App</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>{toggle ? 'ff' : 'False'}</Text>
+      <Button
+        onPress={ToggleTrueFalseText}
+        title='Next Page'
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 100,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
 });
